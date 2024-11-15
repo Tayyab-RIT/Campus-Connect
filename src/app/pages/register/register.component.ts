c1af807c74f01a6dd25188005ea379805af1c18c
Prisha <psm5557@rit.edu>
2024-11-15 18:45:33 +0400

    added login and register pages

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,32 +1,22 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../auth.service';
-import { Router } from '@angular/router';
-import { catchError, of, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css',
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
+  data = {
+    username: '',
+    email: '',
+    password: ''
+  };
 
-  constructor(private authService: AuthService, private router: Router) {}
 
   onRegister() {
-    this.authService.register(this.email, this.password).subscribe({
-      next: (response) => {
-        console.log('Registration successful', response);
-        this.router.navigate(['/home']); // Redirect after successful registration
-      },
-      error: (error) => {
-        console.error('Registration failed', error);
-        // Display a specific error message based on the backend response
-        this.errorMessage =
-          error.error?.message || 'Registration failed. Please try again.';
-      },
-    });
   }
 }
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,32 +1,22 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../auth.service';
-import { Router } from '@angular/router';
-import { catchError, of, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css',
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class SignInComponent {
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
+export class LoginComponent {
+  credentials = {
+    username: '',
+    password: ''
+  };
 
-  constructor(private authService: AuthService, private router: Router) {}
 
-  onLogIn() {
-    this.authService.login(this.email, this.password).subscribe({
-      next: (response) => {
-        console.log('Sign-in successful', response);
-        this.router.navigate(['/home']); // Redirect after successful sign-in
-      },
-      error: (error) => {
-        console.error('Sign-in failed', error);
-        // Display a specific error message based on the backend response
-        this.errorMessage =
-          error.error?.error || 'Sign-in failed. Please try again.';
-      },
-    });
+  onLogin() {
+
   }
 }
